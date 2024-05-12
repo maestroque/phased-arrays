71e54fee332db40d5674ee32d2e375bcb5abc5c2
maestroque <kikasgeorg@ece.auth.gr>
2024-05-12 22:31:36 +0200

    Add triangular 2D phased array radiation pattern scripts

--- triangularArrayRadPattern.m
+++ triangularArrayRadPattern.m
@@ -1,34 +1,35 @@
-function y = triangularArrayRadPattern(Nx, Ny, dx, dy, f, theta0, phi0)
+function y = triangularArrayRadPattern(Nx, Ny, dx, dy, f, u0, v0)
     % Nx, Ny: number of elements in x and y direction
     % dx, dy: distance between elements in x and y direction
     % f: frequency
     % theta0, phi0: direction of main beam
-    
+
     c = 3e8;
     lambda = c/f;
-    
-    % Calculate the position of each element
-    x = (0:Nx-1)*dx;
-    y = (0:Ny-1)*dy;
-    [X, Y] = meshgrid(x, y);
-    
-    % Calculate the distance from each element to the origin
-    R = sqrt(X.^2 + Y.^2);
-    
-    % Calculate the angle of each element
-    theta = atan2(R, ones(Ny, Nx)*R(1, 1));
-    phi = atan2(Y, X);
-    
-    % Calculate the phase difference between each element and the first
-    % element
-    delta = 2*pi/lambda*(R.*cos(theta)*cos(phi0)*sin(theta0) + R.*sin(theta)*sin(phi));
-    
-    % Calculate the array factor
-    AF = 1/Nx/Ny*sum(exp(1i*delta), 'all');
-    
-    % Calculate the radiation pattern
-    y = abs(AF);
+    k = 2*pi/lambda;
 
-    surf(X, Y, y)
+    [u, v] = meshgrid(-1:0.01:1, -1:0.01:1);
+
+    S = zeros(size(u));
+    for r = 1:Nx
+        for l = 1:Ny
+            psi = (dx*(r-1)*u0 + dy*(l-1)*v0);
+            if mod(l - 1, 2) == 0
+                S = S + exp(1j * k * (dx*(r-1) * (u - u0) + dy*(l-1)*(v-v0)));
+            else
+                S = S + exp(1j * k * (dx*(r-1/2)*u + dy*(l-1)*v - psi));
+            end
+        end
+    end
+
+    S = abs(S);
+    S = S / max(S, [], "all");
+    figure;
+    contourf(u, v, S);
+    xlabel('Theta (radians)');
+    ylabel('Normalized Gain');
+    title('Linear Array Radiation Pattern');
+    grid on;
+    axis tight;
 
 end
